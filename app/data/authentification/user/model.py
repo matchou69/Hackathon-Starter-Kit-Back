6183bd070732a8404fe8d4709ebdda410a124cc4
Guilheim Chataing <guilheimchataing@gmail.com>
2023-10-18 11:36:41 +0200

    suppression .db

--- app/data/authentification/user/model.py
+++ app/data/authentification/user/model.py
@@ -1,13 +1,14 @@
 import sqlalchemy
+from sqlalchemy import Column, UUID, String
 
 from shared import db
 
 
 class UserModel(db.Model):
     __tablename__ = "user"
-    id = db.Column(
-        db.UUID, primary_key=True, unique=True, server_default=sqlalchemy.text("gen_random_uuid()"), nullable=False
+    id = Column(
+        UUID, primary_key=True, unique=True, server_default=sqlalchemy.text("gen_random_uuid()"), nullable=False
     )
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-    phone = db.Column(db.String, unique=True)
+    username = Column(String)
+    password = Column(String)
+    phone = Column(String, unique=True)
