3f54b7c6e68cc46665616cedc025f7e76192d7f8
matchou69 <caulfutymathieu@gmail.com>
2023-12-12 15:52:58 +0100

    modifier

--- app/data/auth/controllers/auth_controllers.py
+++ app/data/auth/controllers/auth_controllers.py
@@ -8,7 +8,7 @@ NAME= 'auth'
 
 auth_blueprint = Blueprint(f"{NAME}_auth_blueprint", __name__)
 
-@auth_blueprint.post(f"{NAME}/register")
+"""@auth_blueprint.post(f"{NAME}/register")
 def register():
     data = request.json
     try:
@@ -19,7 +19,38 @@ def register():
     db.session.add(entity)
     db.session.commit()
 
-    return UserSchema().dump(entity), 200
+    return UserSchema().dump(entity), 200"""
+
+
+from flask import jsonify
+
+@auth_blueprint.post(f"{NAME}/register")
+def register():
+    data = request.json
+
+    if 'username' not in data or 'password' not in data:
+        return jsonify({'message': 'Missing username or password'}), 400
+
+    username = data['username']
+    password = data['password']
+
+    # Check if the username already exists in the database
+    existing_user = UserModel.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({'message': 'Username already in use. Please choose a different username.'}), 400
+
+    try:
+        entity: UserModel = UserSchema().load(data)
+    except ValidationError as error:
+        return jsonify({'message': f"The payload doesn't correspond to a valid UserModel: {error}"}), 400
+
+    db.session.add(entity)
+    db.session.commit()
+
+    return jsonify({'message': 'Registration successful', 'user': UserSchema().dump(entity)}), 200
+
+
+
 
 @auth_blueprint.post(f"{NAME}/login")
 def login():
